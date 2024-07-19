2861c2258ca58eec3786fc2e8d399950d3a8b11f
Yashvardhan Singh <draco882003@gmail.com>
2024-07-19 15:07:00 +0530

    Add responsive design

--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,45 @@
 // Handle the question on the `index.html`
-const buttonYes = document.getElementById("button-yes");
-const buttonNo = document.getElementById("button-no");
-const response = document.getElementById("response");
+document.addEventListener("DOMContentLoaded", _ => {
+  const buttonYes = document.getElementById("button-yes");
+  const buttonNo = document.getElementById("button-no");
+  const response = document.getElementById("response");
+  const responseRating = document.getElementById("response-rating");
 
-// Setup the counters
-let yesCount = 0;
-let noCount = 0;
+  // Setup the counters
+  let yesCount = 0;
+  let noCount = 0;
 
-// Record and update the response
-function recordResponse(choice) {
-  if (choice === "yes") {
-    yesCount++;
-  } else if (choice === "no") {
-    noCount++;
+  // Record and update the response
+  function recordResponse(choice) {
+    if (choice === "yes") {
+      yesCount++;
+    } else if (choice === "no") {
+      noCount++;
+    }
+
+    // Get approval rating
+    let { rating, totalVoteCount } = computeRating({ yesCount, noCount });
+    console.log(`Rating: ${rating}`);
+
+    // Update the response
+    response.textContent = `Approval Rating: ${rating}% (${totalVoteCount} votes)`;
   }
 
-  // Get approval rating
-  let { rating, totalVoteCount } = computeRating({ yesCount, noCount });
+  // Compute the approval rating
+  function computeRating(data) {
+    let totalVoteCount = data.yesCount + data.noCount;
+    let rating = (data.yesCount / totalVoteCount * 100).toFixed(2);
+    
+    return { rating, totalVoteCount };
+  }
 
-  // Update the response
-  response.innerText = `Approval Rating: ${rating}% (${totalVoteCount} votes)`;
-}
+  buttonYes.addEventListener("click", _ => {
+    console.log(`Selected: "Yes"!`);
+    recordResponse("yes");
+  });
 
-// Compute the approval rating
-function computeRating(data) {
-  let totalVoteCount = data.yesCount + data.noCount;
-  let rating = (data.yesCount / totalVoteCount * 100).toFixed(2);
-  return { rating, totalVoteCount };
-}
-
-buttonYes.addEventListener("click", _ => {
-  console.log(`Selected: "Yes"!`);
-  recordResponse("yes");
-});
-
-buttonNo.addEventListener("click", _ => {
-  console.log(`Selected "No"!`);
-  recordResponse("no");
+  buttonNo.addEventListener("click", _ => {
+    console.log(`Selected "No"!`);
+    recordResponse("no");
+  });
 });
